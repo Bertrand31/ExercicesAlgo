0fcea7dc8e4495053d05f8fbb7afd1c916e32cee
Bertrand31 <bertrandjun@gmail.com>
2020-03-16 10:39:36 +0100

    Add SubsetSums problem

---
+++ Scala/src/main/scala/InterviewsTraining/SubsetSums.scala
@@ -0,0 +1,22 @@
+// Let A be an array containing both positive and negative integers.
+// Count the number of subsets of A that sum up to a target integer.
+
+object SubsetSums {
+
+  def numberOfSubsets(arr: Array[Int], target: Int): Int = {
+    val runningSums = arr.foldLeft(Array.empty[Int])((acc, item) =>
+      acc :+ (acc.lastOption.getOrElse(0) + item)
+    )
+    val mapOfSums = runningSums.foldLeft(Map.empty[Int, Int])((acc, item) => {
+      val newCount = acc.getOrElse(item, 0) + 1
+      acc + (item -> newCount)
+    })
+    runningSums.foldLeft(0)((acc, item) => acc + mapOfSums.getOrElse(item - target, 0))
+  }
+}
+
+object SubsetSumsApp extends App {
+
+  val arr = Array(10, 5, 1, 2, -1, -1, 7, 1, 2)
+  assert(SubsetSums.numberOfSubsets(arr, 8) == 5)
+}
