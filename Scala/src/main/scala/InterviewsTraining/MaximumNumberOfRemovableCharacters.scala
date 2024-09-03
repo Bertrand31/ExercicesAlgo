76ef7c0fda5ff52f8df813e6f6abc5b65d337ef3
Bertrand31 <bertrandjun@gmail.com>
2024-09-03 22:56:43 +0200

    Improve memory usage of maximumnumberofremovablecharacters

--- Scala/src/main/scala/InterviewsTraining/MaximumNumberOfRemovableCharacters.scala
+++ Scala/src/main/scala/InterviewsTraining/MaximumNumberOfRemovableCharacters.scala
@@ -1,44 +1,39 @@
 import scala.annotation.tailrec
+import scala.collection.mutable.{HashMap, HashSet}
 
 object MaximumNumberOfRemovableCharacters extends App {
 
-  private def getIndices(str: String, subStr: String): Map[Char, IndexedSeq[Int]] =
+  type Indexes = HashMap[Char, HashSet[Int]]
+
+  private def getIndices(str: String, subStr: String): Indexes =
     val subStrSet = subStr.toSet
     str
       .zipWithIndex
       .filter(tpl => subStrSet.contains(tpl._1))
       .groupMap(_._1)(_._2)
+      .view.mapValues(_.to(HashSet)).to(HashMap)
 
-  @tailrec
-  private def getRuns(
-    remaining: List[Char],
-    indexes: Map[Char, IndexedSeq[Int]],
-    soFar: List[List[Int]] = List.empty,
-  ): List[List[Int]] =
-    if remaining.isEmpty then soFar
+  // @tailrec
+  private def runExists(
+    str: String,
+    i: Int,
+    indexes: Indexes,
+    forbidden: HashSet[Int],
+    lastIndex: Int = -1,
+  ): Boolean =
+    if str.sizeIs <= i then true
     else
-      val head :: tail = remaining: @unchecked
-      val currIndexes = indexes(head)
-      val newSoFar = soFar.flatMap(run => currIndexes.flatMap(i =>
-        if i > run.head then Some(i :: run)
-        else None
-      ))
-      getRuns(tail, indexes, newSoFar)
-      
-
-  @tailrec
-  private def getMaximumRemovals(removals: Vector[Int], runs: List[Set[Int]]): Int =
-    if runs.exists(run => !removals.exists(run.contains)) then
-      removals.size
-    else
-      getMaximumRemovals(removals.init, runs)
-
+      val char = str(i)
+      val allowedIndexes = indexes(char).subtractAll(forbidden).filter(_ > lastIndex)
+      allowedIndexes.exists(runExists(str, i + 1, indexes, forbidden, _))
 
   def maximumRemovals(s: String, p: String, removable: Array[Int]): Int =
     val indices = getIndices(s, p)
-    val runs = getRuns(p.tail.toList, indices, indices(p.head).map(List(_)).toList)
-    getMaximumRemovals(removable.toVector, runs.map(_.toSet))
+    (1 to removable.size).findLast(size =>
+      runExists(p, 0, indices, removable.take(size).to(HashSet))
+    ).getOrElse(0)
 
+  // println(maximumRemovals("abcacb", "ab", Array(3, 1, 0)))
   assert(maximumRemovals("abcacb", "ab", Array(3, 1, 0)) == 2)
   assert(maximumRemovals("abcbddddd", "abcd", Array(3, 2, 1, 4, 5, 6)) == 1)
   assert(maximumRemovals("abcab", "abc", Array(0, 1, 2, 3, 4)) == 0)
